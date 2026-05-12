0d801f9034d0969920a9cf7441815a3edb5ffbc6
Joshua Dave Dela Cruz <joshuaxd050@gmail.com>
2026-05-12 15:17:15 +0800

    Lvl5Scene

--- Assets/Scripts/VisionaryUI.cs
+++ Assets/Scripts/VisionaryUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,13 +7,45 @@ public class VisionaryUI : MonoBehaviour
     [SerializeField] Image eyeFill;
     [SerializeField] PlayerMovement player;
 
+    [Header("Warning Shake")]
+    [SerializeField] float shakeMagnitude = 8f;
+    [SerializeField] float shakeDuration  = 0.5f;
+
+    RectTransform eyeRect;
+    Vector2 eyeOrigin;
+
+    void Awake()
+    {
+        eyeRect   = eyeFill != null ? eyeFill.GetComponentInParent<RectTransform>() : null;
+        if (eyeRect != null) eyeOrigin = eyeRect.anchoredPosition;
+    }
+
+    void OnEnable()  => AutoWorldToggle.OnWarning += TriggerShake;
+    void OnDisable() => AutoWorldToggle.OnWarning -= TriggerShake;
+
     void Update()
     {
         if (player == null || eyeFill == null) return;
 
-        if (player.IsOnCooldown)
-            eyeFill.fillAmount = 1f - player.CooldownFraction;
-        else
-            eyeFill.fillAmount = player.VisionFraction;
+        eyeFill.fillAmount = player.IsOnCooldown
+            ? 1f - player.CooldownFraction
+            : player.VisionFraction;
+    }
+
+    void TriggerShake() => StartCoroutine(Shake());
+
+    IEnumerator Shake()
+    {
+        if (eyeRect == null) yield break;
+
+        float elapsed = 0f;
+        while (elapsed < shakeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float strength = Mathf.Lerp(shakeMagnitude, 0f, elapsed / shakeDuration);
+            eyeRect.anchoredPosition = eyeOrigin + Random.insideUnitCircle * strength;
+            yield return null;
+        }
+        eyeRect.anchoredPosition = eyeOrigin;
     }
 }
