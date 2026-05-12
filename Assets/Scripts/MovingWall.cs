84bb40a5dc344c0d358eeffb04ce7492e02e1087
Joshua Dave Dela Cruz <joshuaxd050@gmail.com>
2026-05-12 21:12:14 +0800

    BugFixes

--- Assets/Scripts/MovingWall.cs
+++ Assets/Scripts/MovingWall.cs
@@ -1,34 +1,38 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MovingWall : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] float moveDistance = 5f;
-    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float moveSpeed    = 3f;
 
+    Rigidbody2D rb;
     Vector2 startPos;
     bool movingRight = true;
 
+    void Awake()
+    {
+        rb          = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+    }
+
     void Start()
     {
-        startPos = transform.position;
+        startPos = rb.position;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        MoveWall();
-    }
+        float dir    = movingRight ? 1f : -1f;
+        Vector2 next = rb.position + Vector2.right * dir * moveSpeed * Time.fixedDeltaTime;
 
-    void MoveWall()
-    {
-        float dir = movingRight ? 1f : -1f;
+        if (next.x >= startPos.x + moveDistance) movingRight = false;
+        if (next.x <= startPos.x - moveDistance) movingRight = true;
 
-        transform.Translate(Vector2.right * dir * moveSpeed * Time.deltaTime);
-
-        if (transform.position.x >= startPos.x + moveDistance)
-            movingRight = false;
-
-        if (transform.position.x <= startPos.x - moveDistance)
-            movingRight = true;
+        dir  = movingRight ? 1f : -1f;
+        next = rb.position + Vector2.right * dir * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(next);
     }
 }
